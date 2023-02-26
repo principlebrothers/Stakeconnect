4e02058f3ab7564a50735a94bb81d6b85d6213d5
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-26 21:25:03 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  self.inheritance_column = :type
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true
+  validates :role, presence: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
+  has_secure_password
+
+  def admin?
+    type == 'Admin'
+  end
+
+  def teacher?
+    type == 'Teacher'
+  end
+
+  def parent?
+    type == 'Parent'
+  end
 end
