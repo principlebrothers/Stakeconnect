35b3c504ad2e06cc4ed5929a281def888d402640
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-26 21:24:29 +0000

    Update teacher.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,10 @@
-class Teacher < ApplicationRecord
+class Teacher < User
+  has_many :grades
+  has_many :students, through: :grades
+  has_many :teacher_courses
+  has_many :courses, through: :teacher_courses
+  has_many :administrator_parent_teachers
+  has_many :parents, through: :administrator_parent_teachers
+
+  validates :course_id, presence: true
 end
