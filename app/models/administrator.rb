665d33697f7b3cfc50d01dc0224ddcae6076f707
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-26 21:21:31 +0000

    Update administrator.rb

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,2 +1,12 @@
-class Administrator < ApplicationRecord
+class Administrator < User
+  def self.type
+    %w[HeadTeacher HeadAcademic]
+  end
+
+  has_many :courses
+  has_many :students
+  has_many :administrator_parent_teachers
+  has_many :teachers, through: :administrator_parent_teachers
+  has_many :parents, through: :administrator_parent_teachers
+
 end
