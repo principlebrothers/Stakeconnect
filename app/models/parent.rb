dd83ab847584bbe3562d841678d2b519d6a68873
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-26 21:23:38 +0000

    Update parent.rb

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,2 +1,8 @@
-class Parent < ApplicationRecord
+class Parent < User
+  has_many :students
+  has_many :administrator_parent_teachers
+  has_many :teachers, through: :administrator_parent_teachers
+  has_many :administrators, through: :administrator_parent_teachers
+
+  validates :address, presence: true
 end
