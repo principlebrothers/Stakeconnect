dad3ebeadbd718ec41b8683fb57709481f9c08c3
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-02-28 21:53:24 +0000

    Update 20230226195404_remove_column_from_table.rb

--- db/migrate/20230226195404_remove_column_from_table.rb
+++ db/migrate/20230226195404_remove_column_from_table.rb
@@ -1,5 +1,8 @@
 class RemoveColumnFromTable < ActiveRecord::Migration[7.0]
   def change
     remove_column :students, :grade, :string
+    remove_column :users, :phone, :integer
+    remove_column :users, :password, :string
+    remove_column :users, :password_confirmation, :string
   end
 end
