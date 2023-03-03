2d75b1008b2ba2c484d3ac6805a7a946fe5eb070
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-03 22:50:46 +0000

    Update 20230223204730_create_administrators.rb

--- db/migrate/20230223204730_create_administrators.rb
+++ db/migrate/20230223204730_create_administrators.rb
@@ -2,6 +2,7 @@ class CreateAdministrators < ActiveRecord::Migration[7.0]
   def change
     create_table :administrators do |t|
       t.string :type
+      t.integer :role
 
       t.timestamps
     end
