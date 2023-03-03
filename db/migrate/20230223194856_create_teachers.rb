4c89add9197ea6a341ecc14955359d56b326b651
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-03 22:38:12 +0000

    Update 20230223194856_create_teachers.rb

--- db/migrate/20230223194856_create_teachers.rb
+++ db/migrate/20230223194856_create_teachers.rb
@@ -1,7 +1,14 @@
 class CreateTeachers < ActiveRecord::Migration[7.0]
   def change
     create_table :teachers do |t|
+      t.string :name, null: false
+      t.integer :number, null: false
+      t.string :email, null: false
+      t.string :image, null: false
       t.string :type, null: false
+      t.string :password, null: false
+      t.string :password_confirmation, null: false
+      t.integer :role
       t.timestamps
     end
   end
