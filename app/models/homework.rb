df40874bd1ddf3d345865b73632b011388ab1a9c
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-04 23:41:52 +0000

    Update homework.rb

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,9 +1,8 @@
 class Homework < ApplicationRecord
   belongs_to :course
 
-  validates :date, presence: true
-  validates :question, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :question, presence: true
   validates :course_id, presence: true
 end
