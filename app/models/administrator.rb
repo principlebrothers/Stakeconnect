c4f33408c6b1a9466b9ee8dce44cbd9e765023de
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-04 23:41:01 +0000

    Update administrator.rb

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,10 +1,6 @@
 class Administrator < User
   self.table_name = 'administrators'
 
-  def self.type
-    %w[HeadTeacher HeadAcademic]
-  end
-
   has_many :courses
   has_many :students
   has_many :administrator_parent_teachers
