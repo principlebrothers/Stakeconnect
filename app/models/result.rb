d96403960988e44345974d5387a057254243cffc
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-05 23:11:10 +0000

    Update result.rb

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,12 @@
 class Result < ApplicationRecord
   self.inheritance_column = :type # enable single table inheritance
-  
+
   belongs_to :course
+  belongs_to :student
 
   validates :course_id, presence: true
+  validates :student_id, presence: true
   validates :date, presence: true
   validates :score, presence: true
+
 end
