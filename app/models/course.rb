2c1f8040dfdb92876a644003b972bb2ac091016b
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-06 00:10:40 +0000

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,10 @@ class Course < ApplicationRecord
   belongs_to :grade
   has_many :teacher_courses
   has_many :teachers, through: :teacher_courses
+  has_many :results
+  has_many :homeworks
+  has_many :course_reports
+  has_many :reports, through: :course_reports
 
   validates :name, presence: true
   validates :semester, presence: true, numericality: { only_integer: true }
