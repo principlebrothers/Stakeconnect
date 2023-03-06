fa4ff564f88a46afbe128796d2221428e01f8568
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-06 00:10:21 +0000

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,9 @@ class Student < ApplicationRecord
   belongs_to :parent
   belongs_to :grade
   belongs_to :administrator
-  has_many :courses, through: :grades
+  has_many :courses, through: :grade
+  has_many :results
+  has_many :reports
 
   validates :name, presence: true
   validates :parent_id, presence: true
