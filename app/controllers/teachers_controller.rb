31454da74ac36e2d6c69250813604817d2c4552e
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-06 00:11:47 +0000

    Update teachers_controller.rb

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -35,7 +35,11 @@ class TeachersController < ApplicationController
 
   # DELETE /teachers/1
   def destroy
-    @teacher.destroy
+    if @teacher.destroy
+      render json: {message: "Teacher deleted successfully"}, status: :ok
+    else
+      render json: @teacher.errors, status: :unprocessable_entity
+    end
   end
 
   private
