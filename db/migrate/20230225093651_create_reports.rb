c26bbf3c3471429feb9bfa8cc5da9ab16d92a6dc
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-06 00:21:44 +0000

    Update 20230225093651_create_reports.rb

--- db/migrate/20230225093651_create_reports.rb
+++ db/migrate/20230225093651_create_reports.rb
@@ -2,8 +2,8 @@ class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
       t.date :date
+      t.text :remark
       t.references :student, null: false, foreign_key: true
-      t.references :result, null: false, foreign_key: true
 
       t.timestamps
     end
