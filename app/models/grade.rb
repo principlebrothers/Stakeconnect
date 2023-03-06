fbbfc973304828ad01c358057184416e6be0cca9
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-06 00:09:46 +0000

    Update grade.rb

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,6 +1,7 @@
 class Grade < ApplicationRecord
   belongs_to :teacher
   has_many :students
+  has_many :courses
 
   validates :teacher_id, presence: true
   validates :grade_num, presence: true, uniqueness: true, numericality: { only_integer: true }
