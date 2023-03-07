a12bfacd62dd911f64039efc23541d838062a456
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-07 16:05:49 +0000

    Add devise set_up to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   self.inheritance_column = :type
 
   validates :name, presence: true
@@ -7,7 +14,10 @@ class User < ApplicationRecord
   validates :image, presence: true
   # validates :role, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+  def jwt_payload
+    super
+  end
 
   def admin?
     type == 'Admin'
