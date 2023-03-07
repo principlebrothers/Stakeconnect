1c57670516a5e7bd6be8cce35eb24dde6e834dd6
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-07 16:02:54 +0000

    Update the user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :number, :email, :password, :password_confirmation, :image, :role)
+    params.require(:user).permit(:name, :number, :email, :image, :role, :password)
   end
 end
