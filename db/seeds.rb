a767000df522690275e181c034667abd577668a3
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-07 16:07:48 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,26 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 # Create Administrators
-admin_1 = Administrator.create(number: '0555652356', name: 'Niger', email: 'good@email.com', password: 'Ajaguraja@24@@',
-                               password_confirmation: 'Ajaguraja@24@@', image: 'image')
-admin_2 = Administrator.create(number: '0555651245', name: 'Koo', email: 'agya@email.com', password: 'Ajaguraja@24@@@',
-                               password_confirmation: 'Ajaguraja@24@@@', image: 'image')
+admin_1 = Administrator.create(number: '0555652356', name: 'Niger', email: 'good@email.com',
+                               password: 'Ajaguraja@24@@', image: 'image')
+admin_2 = Administrator.create(number: '0555651245', name: 'Koo', email: 'agya@email.com',
+                               password: 'Ajaguraja@24@@@', image: 'image')
 
 # Create Teachers
-teacher_1 = Teacher.create(name: 'John Defoe', number: '0505651234', email: 'defoe@email.com', password: 'Pass@In1243',
-                           password_confirmation: 'Pass@In1243', image: 'image')
-teacher_2 = Teacher.create(name: 'Obrempong Sikani', number: '0595651234', email: 'obremp@email.com', password: 'Obemp4@m',
-                           password_confirmation: 'Obemp4@m', image: 'image')
+teacher_1 = Teacher.create(name: 'John Defoe', number: '0505651234', email: 'defoe@email.com',
+                           password: 'Pass@In1243', image: 'image')
+teacher_2 = Teacher.create(name: 'Obrempong Sikani', number: '0595651234', email: 'obremp@email.com',
+                           password: 'Obemp4@m', image: 'image')
 teacher_3 = Teacher.create(name: 'Kwame Nkrumah', number: '0277651234', email: 'kwamenkrumah@emai.com',
-                           password: 'Kwame@Nkrumah', password_confirmation: 'Kwame@Nkrumah', image: 'image')
+                           password: 'Kwame@Nkrumah', image: 'image')
 
 # Create Parents
-parent_1 = Parent.create(name: 'Parent', number: '0554651234', email: 'parents@email.com', password: 'Pass@@In13',
-                         password_confirmation: 'Pass@@In13', image: 'image', address: '27 Oxford St.')
-parent_2 = Parent.create(name: 'Mr. Odei', number: '0254653234', email: 'response@email.com', password: 'Parass@@In13',
-                         password_confirmation: 'Parass@@In13', image: 'image', address: '100 Main St.')
+parent_1 = Parent.create(name: 'Parent', number: '0554651234', email: 'parents@email.com',
+                         password: 'Pass@@In13', image: 'image', address: '27 Oxford St.')
+parent_2 = Parent.create(name: 'Mr. Odei', number: '0254653234', email: 'response@email.com',
+                         password: 'Parass@@In13', image: 'image', address: '100 Main St.')
 parent_3 = Parent.create(name: 'Augustine B.', number: '0554651204', email: 'augustine@email.com',
-                         password: 'Augustine@@In1', password_confirmation: 'Augustine@@1', image: 'image', address: '27 Augustine St.')
+                         password: 'Augustine@@1', image: 'image', address: '27 Augustine St.')
 
 # Create Administrators_parent_teachers
 admin_parent_teacher_1 = AdministratorParentTeacher.create(administrator_id: admin_1.id, parent_id: parent_1.id,
