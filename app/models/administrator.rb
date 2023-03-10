ae1426cc2c8bbd89a2bc001e7053ce9e53e46fd4
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-10 23:54:23 +0000

    Add devise to model

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,15 +1,25 @@
 class Administrator < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :number, presence: true, uniqueness: true
   validates :image, presence: true
   # validates :role, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   has_many :courses
   has_many :students
   has_many :administrator_parent_teachers
   has_many :teachers, through: :administrator_parent_teachers
   has_many :parents, through: :administrator_parent_teachers
+
+  def jwt_payload
+    super
+  end
 end
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,4 +1,11 @@
 class Parent < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :number, presence: true, uniqueness: true
@@ -6,10 +13,13 @@ class Parent < ApplicationRecord
   validates :address, presence: true
   # validates :role, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   has_many :students
   has_many :administrator_parent_teachers
   has_many :teachers, through: :administrator_parent_teachers
   has_many :administrators, through: :administrator_parent_teachers
+
+  def jwt_payload
+    super
+  end
 end
