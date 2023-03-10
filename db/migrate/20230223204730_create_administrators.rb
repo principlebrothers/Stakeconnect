399522908c9f954dcbc7f3b4681dea11bd1e5816
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-10 23:55:12 +0000

    Update 20230223204730_create_administrators.rb

--- db/migrate/20230223204730_create_administrators.rb
+++ db/migrate/20230223204730_create_administrators.rb
@@ -5,7 +5,6 @@ class CreateAdministrators < ActiveRecord::Migration[7.0]
       t.integer :number, null: false
       t.string :email, null: false
       t.string :image, null: false
-      t.string :password_confirmation, null: false
       t.integer :role
 
       t.timestamps
