3cfda9ef9c45d83522c2f55428ca0ae75d540255
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-10 23:54:57 +0000

    Update 20230223194950_create_parents.rb

--- db/migrate/20230223194950_create_parents.rb
+++ db/migrate/20230223194950_create_parents.rb
@@ -5,14 +5,11 @@ class CreateParents < ActiveRecord::Migration[7.0]
       t.integer :number, null: false
       t.string :email, null: false
       t.string :image, null: false
-      t.string :password_confirmation, null: false
       t.integer :role
       t.text :address, null: false
 
       t.timestamps
     end
 
-    add_index :parents, :email, unique: true
-    add_index :parents, :number, unique: true
   end
 end
