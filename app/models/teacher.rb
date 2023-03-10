ff2c3946cd1bcb810a96a97dffe8bc52986de176
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-10 10:49:45 +0000

    Update teacher.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,11 @@
-class Teacher < User
-  self.table_name = 'teachers'
+class Teacher < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true
+  validates :image, presence: true
+  # validates :role, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   has_many :grades
   has_many :students, through: :grades
