521bdbf6f13a3ed17bcc707183ab925f3537bdfd
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-10 10:49:18 +0000

    Update parent.rb

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,10 +1,15 @@
-class Parent < User
-  self.table_name = 'parents'
+class Parent < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :number, presence: true, uniqueness: true
+  validates :image, presence: true
+  validates :address, presence: true
+  # validates :role, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   has_many :students
   has_many :administrator_parent_teachers
   has_many :teachers, through: :administrator_parent_teachers
   has_many :administrators, through: :administrator_parent_teachers
-
-  validates :address, presence: true
 end
