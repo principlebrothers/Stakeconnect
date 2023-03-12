ebf904413f56bb329d9d911b3b243fd1c71ccd41
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-12 10:40:28 +0000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { message: exception.message }, status: :unauthorized
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { message: exception.message }, status: :not_found
+  end
+
+  private
+
+  def current_user
+    current_administrator || current_teacher || current_parent
+  end
 end
