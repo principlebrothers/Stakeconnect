0d03fdde6a96f41d1a38439761f3c107048fca78
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-12 10:37:11 +0000

    Delete administrator reference to course and student

--- db/migrate/20230225094249_add_ref_to_table.rb
+++ db/migrate/20230225094249_add_ref_to_table.rb
@@ -1,6 +1,5 @@
 class AddRefToTable < ActiveRecord::Migration[7.0]
   def change
-    add_reference :students, :administrator, foreign_key: true
     add_reference :students, :parent, foreign_key: true
     add_reference :students, :grade, foreign_key: true
   end
