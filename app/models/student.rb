8f12b6edb48a10fbd70cfab12afa6551a920d30b
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-12 10:42:03 +0000

    Remove administrator association and foreign key

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :parent
   belongs_to :grade
-  belongs_to :administrator
   has_many :courses, through: :grade
   has_many :results
   has_many :reports
@@ -9,6 +8,5 @@ class Student < ApplicationRecord
   validates :name, presence: true
   validates :parent_id, presence: true
   validates :image, presence: true
-  validates :administrator_id, presence: true
   validates :grade_id, presence: true
 end
