2c954df2a93e2bc2b3f13ff357ad6192e60b18f5
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-13 18:57:46 +0000

    Update hosts details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.host << 'stakeconnect-production.up.railway.app'
+  config.hosts << 'stakeconnect-production.up.railway.app'
   Rails.secret_key_base = ENV['SECRET_KEY_BASE']
 
   # Code is not reloaded between requests.
