aea73fbdd7e382655506a929ae141e121443f0f1
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-03-15 09:41:04 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << 'stakeconnect-production.up.railway.app'
-  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  config.secret_key_base = Rails.application.credentials.fetch(:secret_key_base)
 
   # Code is not reloaded between requests.
   config.cache_classes = true
